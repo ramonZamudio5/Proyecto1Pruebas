15f0a319abe74d38e1e28ad952c958496f518cb4
AngelServin-ops <angel.servindelamora240831@potros.itson.edu.mx>
2026-02-18 22:54:37 -0700

    capas

---
+++ ProyectoListaReproduccionMusical/src/main/java/PersistenciaUs/CatalogoMusical.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PersistenciaUs;
+
+import dominio.Cancion;
+import dominio.Playlist;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author angel
+ */
+public class CatalogoMusical {
+
+
+}
